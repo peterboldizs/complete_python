5f979897bf176bd74a1ec282c43a5a0fc76d66e2
Boldizs Péter <boldizs.peter@fornax.hu>
2017-01-13 11:17:33 +0100

    ch8 full gui

--- ch8/guiscrape.py
+++ ch8/guiscrape.py
@@ -7,13 +7,86 @@ import os
 from bs4 import BeautifulSoup
 import requests
 
+config = {}
+
 
 def fetch_url():
-    pass
+    url = _url.get()
+    config['images'] = []
+    _images.set(())
+    try:
+        page = requests.get(url)
+    except requests.RequestException as rex:
+        _sb(str(rex))
+    else:
+        soup = BeautifulSoup(page.content, 'html.parser')
+        images = _fetch_images(soup, url)
+        if images:
+            _images.set(tuple(img['name'] for img in images))
+            _sb('images found: {}'.format(len(images)))
+        else:
+            _sb('no images found.')
+        config['images'] = images
+
+
+def _fetch_images(soup, base_url):
+    images = []
+    for img in soup.find_all('img'):
+        src = img.get('src')
+        img_url = (
+            '{base_url}/{src}'.format(base_url=base_url, src=src)
+        )
+        name = img_url.split('/')[-1]
+        images.append(dict(name=name, url=img_url))
+    return images
 
 
 def save():
-    pass
+    if not config['images']:
+        _alert('no images to save')
+        return
+
+    if _save_method.get() == 'img':
+        dirname = filedialog.askdirectory(mustexist=True)
+        _save_images(dirname)
+    else:
+        filename = filedialog.asksaveasfilename(
+            initialfile='images.json',
+            filetypes=[('JSON', '.json')]
+        )
+        _save_json(filename)
+
+
+def _save_images(dirname):
+    if dirname and config.get('images'):
+        for img in config['images']:
+            img_data = requests.get(img['url']).content
+            filename = os.path.join(dirname, img['name'])
+            with open(filename, 'wb') as f:
+                f.write(img_data)
+        _alert('Done')
+
+
+def _save_json(filename):
+    if filename and config.get('images'):
+        data = {}
+        for img in config['images']:
+            img_data = requests.get(img['url']).content
+            b64_img_data = base64.b64encode(img_data)
+            str_img_data = b64_img_data.decode('utf-8')
+            data[img['name']] = str_img_data
+        with open(filename, 'w') as ijson:
+            ijson.write(json.dumps(data))
+        _alert('Done')
+
+
+def _sb(msg):
+    _status_msg.set(msg)
+
+
+def _alert(msg):
+    messagebox.showinfo(message=msg)
+
 
 if __name__ == "__main__":
     _root = Tk()
@@ -72,5 +145,6 @@ if __name__ == "__main__":
     _status.grid(row=0, column=0, sticky=(E, W))
 
     _root.mainloop()
+
 
 
