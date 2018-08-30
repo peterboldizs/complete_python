aab1ab12d439c1453ef3979554e7f0f9d1842dde
Peter Boldizs <peter.boldizs@gmail.com>
2018-08-30 20:49:34 +0200

    split var and print

---
+++ ch00_intro/printing.py
@@ -0,0 +1,26 @@
+print("Hello")
+print('Hello world')
+print("Hello it's time")
+greet = "Hello"
+nname = 'Peter'
+print(greet + " " + nname)
+bigstring = "This is \n a big string \n in more llines"
+tabbed = '1\t2\t3\t4'
+
+spl = """split
+over
+several
+lines"""
+sp2 = """split
+\tover
+\t\tseveral
+\t\t\tlines"""
+
+pet = '''the owner said "He \'s resting"'''
+pet2 = """the owner said "He \'s resting" """
+print(bigstring)
+print(tabbed)
+print(spl)
+print(sp2)
+print(pet)
+print(pet2)
