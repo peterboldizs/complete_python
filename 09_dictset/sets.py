82013ce27ee12adc07bde9a8e46bf3742b548e0e
Peter Boldizs <extern.boldizs_peter@allianz.de>
2018-09-23 11:56:19 +0200

    io text mode and binary mode

--- 09_dictset/sets.py
+++ 09_dictset/sets.py
@@ -17,23 +17,27 @@ print("my_set: {}".format(my_set))
 #  my_set2 = set([7, 11, 13, 17])
 my_set1 = {11, 13}
 my_set2 = set([7, 11, 13, 17])
+my_set3 = {2, 3, 3, 5, 5, 7}
+my_set4 = set(["A", "B", "C", "A"])
+my_set5 = {"E", "F", "G"}
 print("my_set1: {}".format(my_set1))
 print("my_set2: {}".format(my_set2))
+print("my_set3: {}".format(my_set3))
+print("my_set4: {}".format(my_set4))
+print("my_set5: {}".format(my_set5))
 print("my_set1.issubset(my_set2): {}".format(my_set1.issubset(my_set2)))
-#print(my_set | my_set2)
+# print(my_set | my_set2)
 print("difference: {}".format(my_set.difference(my_set2)))
 my_set.difference_update(my_set2)
 print("difference_update: {}".format(my_set))
 my_set.symmetric_difference(my_set2)
 print("symmetric_difference: {}".format(my_set))
-#print(my_set + my_set2)
+# print(my_set + my_set2)
 print("union: {}".format(my_set.union(my_set2)))
-#print(my_set - my_set2)
+# print(my_set - my_set2)
 print("intersection: {}".format(my_set.intersection(my_set2)))
-my_set3 = {2, 3, 3, 5, 5, 7}
-print(my_set3)
 
-my_set4 = set(["A", "B", "C", "A"])
+print(my_set3)
 print(my_set4)
 my_set4.add("D")
 my_set4.add("C")
@@ -41,7 +45,6 @@ my_set4.add("C")
 for i in my_set4:
     print(i)
     
-my_set5 = {"E", "F", "G"}
 print("union: {}".format(my_set4.union(my_set5)))
 print("sorted union: {}".format(sorted(my_set4.union(my_set5))))
 print("intersection: {}".format(my_set2.intersection(my_set3)))
