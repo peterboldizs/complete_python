f66b3c398ea58449f4746d031d2d4d4906af4303
Peter Boldizs <extern.boldizs_peter@allianz.de>
2018-10-06 13:41:00 +0200

    finished OOP section

---
+++ 12_oop/ducks.py
@@ -0,0 +1,63 @@
+class Bird(object):
+
+    def __init__(self):
+        self._feather = "feather"
+
+
+class Wing(object):
+
+    def __init__(self, ratio):
+        self.ratio = ratio
+
+    def fly(self):
+        if self.ratio > 1:
+            print("Weee, this is fun")
+        elif self.ratio == 1:
+            print("This is hard work, but I'm flying")
+        else:
+            print("I think I'll just walk")
+
+
+class Duck(Bird):
+
+    def __init__(self):
+        self._wing = Wing(1.8)
+
+    def walk(self):
+        print("Waddle, waddle, waddle")
+
+    def swim(self):
+        print("Come on it, the water's lovely")
+
+    def quack(self):
+        print("Quack quack")
+
+    def fly(self):
+        self._wing.fly()
+
+
+class Penguin(Bird):
+
+    def walk(self):
+        print("Waddle, waddle, I waddle too")
+
+    def swim(self):
+        print("Come on in, but it's a bit chilly this far South")
+
+    def quack(self):
+        print("Are you 'avin' a larf? I'm a penguin!")
+
+
+def test_bird(bird):
+    bird.walk()
+    bird.swim()
+    bird.quack()
+
+
+if __name__ == '__main__':
+    donald = Duck()
+    test_bird(donald)
+    donald.fly()
+
+    percy = Penguin()
+    test_bird(percy)
