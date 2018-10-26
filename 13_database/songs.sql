bda3f0c851fb4c952ae9a6f0d3855b37716195e0
Peter Boldizs <extern.boldizs_peter@allianz.de>
2018-10-26 18:36:50 +0200

    this is the new course structure

--- 13_database/songs.sql
+++
@@ -1,7 +0,0 @@
-select artists.name, albums.name, songs.title, songs.track from songs
-inner join albums on songs.album = albums._id
-inner join artists on albums.artist = artists._id
--- where albums.name = "Brothers"
--- where songs.title like "%doctor%"
-where artists.name like "%jefferson%"
-order by artists.name, albums.name, songs.track
--- 14_gencomplambda/filtertest.py
+++
@@ -1,9 +0,0 @@
-food = ["apple", "maple", "egg", "pork", "beef"]
-
-
-def no_egg(food_list: list):
-    return "egg" not in food_list
-
-
-eggless = list(filter(no_egg, food))
-print(eggless)
