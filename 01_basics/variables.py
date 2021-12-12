ac23208d0e53692ff3a2d36699bd0a457347a518
peterboldizs <peter.boldizs@gmail.com>
2021-12-12 14:03:01 +0100

    from vs code

--- 01_basics/variables.py
+++ 01_basics/variables.py
@@ -9,6 +9,7 @@ print(strvar + ' ' + _strvar)
 #print(strvar + ' ' + intvar) TypeError: Can't convert 'int' object to str implicitly
 print(intvar * _intvar)
 
+# this is a comment from the online VS Code editor
 # now with input
 inp = input("please enter your name: ")
 print(greeting + ' ' + inp)
